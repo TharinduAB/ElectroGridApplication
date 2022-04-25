68f059eefcb207010e903511af542dd2f1ac2d58
Ishara Wanninayake <ishani9799@gmail.com>
2022-04-26 02:48:32 +0530

    Create inquiry.java and inquiryService.java files

---
+++ src/com/InquiryService.java
@@ -0,0 +1,81 @@
+package com;
+
+import model.Inquiry;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/inquiries")
+public class InquiryService
+{
+	 Inquiry inquiryObj = new Inquiry();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readInquiries()
+	 {
+			return inquiryObj.readInquiries();
+	 }
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertInquiries(
+	@FormParam("name") String name,
+	 @FormParam("email") String email,
+	 @FormParam("contactNumber") String contactNumber,
+	 @FormParam("address") String address,
+	 @FormParam("inquiryType") String inquiryType,
+	 @FormParam("message") String message)
+	 
+	{
+	 String output = inquiryObj.insertInquiries(name, email, contactNumber, address, inquiryType, message);
+	return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateInquiries(String inquiryData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject inquiryObject = new JsonParser().parse(inquiryData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String inquiryID = inquiryObject.get("inquiryID").getAsString();
+	 String name = inquiryObject.get("name").getAsString();
+	 String email = inquiryObject.get("email").getAsString();
+	 String contactNumber = inquiryObject.get("contactNumber").getAsString();
+	 String address = inquiryObject.get("address").getAsString();
+	 String inquiryType = inquiryObject.get("inquiryType").getAsString();
+	 String message = inquiryObject.get("message").getAsString();
+	 String output = inquiryObj.updateInquiries(inquiryID, name, email, contactNumber, address, inquiryType, message);
+	return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteInquiries(String inquiryData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(inquiryData, "", Parser.xmlParser());
+
+	//Read the value from the element <requestId>
+	 String inquiryID = doc.select("inquiryID").text();
+	 String output = inquiryObj.deleteInquiries(inquiryID);
+	return output;
+	}
+	
+}
---
+++ src/model/Inquiry.java
@@ -0,0 +1,202 @@
+package model;
+
+import java.sql.*;
+
+public class Inquiry
+{ 
+	//A common method to connect to the DB
+	private Connection connect()
+	 {
+		 Connection con = null;
+		 
+		 try
+		 {
+		 Class.forName("com.mysql.jdbc.Driver");
+		
+		 //Provide the correct details: DBServer/DBName, username, password
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "root");
+		 }
+		 catch (Exception e)
+		 {e.printStackTrace();}
+		 
+		 return con;
+	 }
+	
+	//Insert
+	public String insertInquiries(String name, String email, String contactNumber, String address, String inquiryType, String message)
+	 {
+		 String output = "";
+		 
+		 try
+		 {
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for inserting."; }
+			 
+			 // create a prepared statement
+			 String query = " insert into inquiries (`inquiryID`,`name`,`email`,`contactNumber`,`address`,`inquiryType`,`message`)" + " values (?, ?, ?, ?, ?, ?, ?)";
+			 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, name);
+			 preparedStmt.setString(3, email);
+			 preparedStmt.setString(4, contactNumber);
+			 preparedStmt.setString(5, address);
+			 preparedStmt.setString(6, inquiryType);
+			 preparedStmt.setString(7, message);
+			 
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 
+			 output = "Inserted successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while inserting the inquiry connection request.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+	}
+	
+	public String readInquiries()
+	{
+		 String output = "";
+		 
+		 try
+		 {
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for reading."; }
+			 
+			 // Prepare the html table to be displayed
+			 output = "<table border='1'><tr><th>Inquiry ID</th><th> Name</th><th>Email</th>" +
+					 "<th>Contact Number</th>" +
+					 "<th>Address</th>" +
+					 "<th>Inquiry Type</th>" +
+					 "<th>Message</th></tr>";
+			
+			 String query = "select * from inquiries";
+			 Statement stmt = con.createStatement();
+			 ResultSet rs = stmt.executeQuery(query);
+			 
+			 // iterate through the rows in the result set
+			 while (rs.next())
+			 {
+				 String inquiryID = Integer.toString(rs.getInt("inquiryID"));
+				 String name = rs.getString("name");
+				 String email = rs.getString("email");
+				 String contactNumber = rs.getString("contactNumber");
+				 String address = rs.getString("address");
+				 String inquiryType = rs.getString("inquiryType");
+				 String message = rs.getString("message");
+				 
+				 
+				 // Add into the html table
+				 output += "<tr><td>" +  inquiryID + "</td>";
+				 output += "<td>" + name + "</td>";
+				 output += "<td>" + email + "</td>";
+				 output += "<td>" + contactNumber + "</td>";
+				 output += "<td>" + address + "</td>";
+				 output += "<td>" + inquiryType + "</td>";
+				 output += "<td>" + message + "</td>";
+				 
+				 
+				 // buttons
+				 //output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>" + "<td><form method='post' action='inquiryrequests.jsp'>" + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>" + "<input name='inquiryId' type='hidden' value='" + inquiryID + "'>" + "</form></td></tr>";
+			 }
+			 con.close();
+			 // Complete the html table
+			 output += "</table>";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while reading the inquiries.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+	}
+	
+	//update
+	public String updateInquiries(String ID, String nameIn, String emailIn, String contactNumberIn, String addressIn, String inquiryTypeIn, String messageIn)
+	{
+		 String output = "";
+		 
+		 try
+		 {
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for updating."; }
+			 
+			 // create a prepared statement
+			 String query = "UPDATE inquiries SET name=?,email=?,contactNumber=?,address=?,inquiryType=?,message=? WHERE inquiryID=?";
+			 
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setString(1, nameIn);
+			 preparedStmt.setString(2, emailIn);
+			 preparedStmt.setString(3, contactNumberIn);
+			 preparedStmt.setString(4, addressIn);
+			 preparedStmt.setString(5, inquiryTypeIn);
+			 preparedStmt.setString(6, messageIn);
+			 preparedStmt.setInt(7, Integer.parseInt(ID));
+			 
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 
+			 output = "Updated successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while updating the inquiry connection request.";
+			 System.err.println(e.getMessage());
+		 }
+		 
+		 return output;
+		 
+	}
+	
+	// delete
+	public String deleteInquiries(String inquiryID )
+	{
+		 String output = "";
+		 
+		 try
+		 {
+		 
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for deleting."; }
+			 
+			 // create a prepared statement
+			 String query = "delete from inquiries where inquiryID=?";
+			 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setInt(1, Integer.parseInt(inquiryID ));
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 
+			 output = "Deleted successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while deleting the inquiry.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+		 }
+	}

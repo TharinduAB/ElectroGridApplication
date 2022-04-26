64dfb520ed2b3649780d71d3dc36457704dcc48c
sadishawikum <sadishavikum@gmail.com>
2022-04-26 23:24:56 +0530

    format and refactored the code

--- src/com/CardtService.java
+++ src/com/CardtService.java
@@ -1,79 +1,86 @@
 package com;
-// REST Service
-import javax.ws.rs.*; 
-import javax.ws.rs.core.MediaType; 
+
 //For JSON
 import com.google.gson.*;
-
+// REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import model.Card;
-
 //For XML
-import org.jsoup.*; 
-import org.jsoup.parser.*; 
-import org.jsoup.nodes.Document; 
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.*;
+
 @Path("/Card")
- 
-public class CardtService 
-{ 
-Card cardObj = new Card(); 
-@GET
-@Path("/") 
-@Produces(MediaType.TEXT_HTML) 
-public String readcard() 
- { 
-	return cardObj.readCardData(); 
- } 
+public class CardtService {
 
-@POST
-@Path("/") 
-@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
-@Produces(MediaType.TEXT_PLAIN) 
-public String insertCardData(@FormParam("cardHolderName") String cardHolderName, 
- @FormParam("cardNo") String cardNo, 
- @FormParam("Month") String Month, 
- @FormParam("Year") String Year, 
-@FormParam("ccvNo") String ccvNo)
-{ 
- String output = cardObj.insertCardData(cardHolderName, cardNo, Month, Year,ccvNo); 
-return output; 
-}
+  Card cardObj = new Card();
 
-@PUT
-@Path("/") 
-@Consumes(MediaType.APPLICATION_JSON) 
-@Produces(MediaType.TEXT_PLAIN) 
-public String updateCard(String cardData) 
-{ 
-//Convert the input string to a JSON object 
- JsonObject cardObject = new JsonParser().parse(cardData).getAsJsonObject(); 
-//Read the values from the JSON object
- String cardID = cardObject.get("cardID").getAsString(); 
- String cardHolderName = cardObject.get("cardHolderName").getAsString(); 
- String cardNo = cardObject.get("cardNo").getAsString(); 
- String Month = cardObject.get("Month").getAsString(); 
- String Year = cardObject.get("Year").getAsString();
- String ccvNo = cardObject.get("ccvNo").getAsString(); 
- String output = cardObj.updateCard(cardID, cardHolderName, cardNo, Month, Year,ccvNo); 
-return output; 
-}
+  @GET
+  @Path("/")
+  @Produces(MediaType.TEXT_HTML)
+  public String readcard() {
+    return cardObj.readCardData();
+  }
 
-@DELETE
-@Path("/") 
-@Consumes(MediaType.APPLICATION_XML) 
-@Produces(MediaType.TEXT_PLAIN) 
-public String deleteCard(String itemData) 
-{ 
-//Convert the input string to an XML document
- Document doc = Jsoup.parse(itemData, "", Parser.xmlParser()); 
- 
-//Read the value from 
- String cardID = doc.select("cardID").text(); 
- String output = cardObj.deleteCard(cardID); 
-return output; 
-}
+  @POST
+  @Path("/")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  @Produces(MediaType.TEXT_PLAIN)
+  public String insertCardData(
+    @FormParam("cardHolderName") String cardHolderName,
+    @FormParam("cardNo") String cardNo,
+    @FormParam("Month") String Month,
+    @FormParam("Year") String Year,
+    @FormParam("ccvNo") String ccvNo
+  ) {
+    String output = cardObj.insertCardData(
+      cardHolderName,
+      cardNo,
+      Month,
+      Year,
+      ccvNo
+    );
+    return output;
+  }
 
+  @PUT
+  @Path("/")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.TEXT_PLAIN)
+  public String updateCard(String cardData) {
+    //Convert the input string to a JSON object
+    JsonObject cardObject = new JsonParser().parse(cardData).getAsJsonObject();
 
+    //Read the values from the JSON object
+    String cardID = cardObject.get("cardID").getAsString();
+    String cardHolderName = cardObject.get("cardHolderName").getAsString();
+    String cardNo = cardObject.get("cardNo").getAsString();
+    String Month = cardObject.get("Month").getAsString();
+    String Year = cardObject.get("Year").getAsString();
+    String ccvNo = cardObject.get("ccvNo").getAsString();
+    String output = cardObj.updateCard(
+      cardID,
+      cardHolderName,
+      cardNo,
+      Month,
+      Year,
+      ccvNo
+    );
+    return output;
+  }
 
+  @DELETE
+  @Path("/")
+  @Consumes(MediaType.APPLICATION_XML)
+  @Produces(MediaType.TEXT_PLAIN)
+  public String deleteCard(String itemData) {
+    //Convert the input string to an XML document
+    Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
 
-
+    //Read the value from
+    String cardID = doc.select("cardID").text();
+    String output = cardObj.deleteCard(cardID);
+    return output;
+  }
 }
--- src/model/Card.java
+++ src/model/Card.java
@@ -1,168 +1,176 @@
 package model;
-import java.sql.*; 
+
+import java.sql.*;
+
 public class Card {
 
-	private Connection connect() 
-	{ 
-			Connection con = null; 
-			
-			try
-	{ 
-				Class.forName("com.mysql.jdbc.Driver"); 
- 
-				//Provide the correct details: DBServer/DBName, username, password 
-				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Grid", "root", ""); 
-	} 
-			catch (Exception e) 
-			{e.printStackTrace();} 
- 
-			return con; 
- }
+  private Connection connect() {
+    Connection con = null;
 
-public String insertCardData(String name, String cardno, String month, String year,String ccvno) 
- { 
- String output = ""; 
- try
- { 
- Connection con = connect(); 
- if (con == null) 
- {return "Error while connecting to the database for inserting."; } 
- // create a prepared statement
- String query = " insert into carddata (`cardID`,`cardHolderName`,`cardNo`,`Month`,`Year`,`ccvNo`)"
- + " values (?, ?, ?, ?, ? , ?)"; 
- PreparedStatement preparedStmt = con.prepareStatement(query); 
- // binding values
- preparedStmt.setInt(1, 0); 
- preparedStmt.setString(2, name); 
- preparedStmt.setString(3, cardno); 
- preparedStmt.setString(4, month);
- preparedStmt.setString(5, year);
- preparedStmt.setString(6, ccvno);
- preparedStmt.execute(); 
- con.close(); 
- output = "Inserted successfully"; 
- } 
- catch (Exception e) 
- { 
- output = "Error while inserting the carddata."; 
- System.err.println(e.getMessage()); 
- } 
- return output; 
- } 
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
 
+      //Provide the correct details: DBServer/DBName, username, password
+      con =
+        DriverManager.getConnection(
+          "jdbc:mysql://localhost:3306/Grid",
+          "root",
+          ""
+        );
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-public String readCardData() 
- { 
- String output = ""; 
- try
- { 
- Connection con = connect(); 
- if (con == null) 
- {return "Error while connecting to the database for reading."; } 
- // Prepare the html table to be displayed
- output = "<table border='1'><tr><th>Card ID</th><th>Card Holder Name</th><th>Card No</th>" + "<th>Month</th>" +
- "<th>Year</th>" + 
- "<th>CCV No</th>" +
- "<th>Update</th><th>Remove</th></tr>"; 
- 
- String query = "select * from carddata"; 
- Statement stmt = con.createStatement(); 
- ResultSet rs = stmt.executeQuery(query); 
- // iterate through the rows in the result set
- while (rs.next()) 
- { 
- String cardID = Integer.toString(rs.getInt("cardID")); 
- String cardHolderName = rs.getString("cardHolderName"); 
- String cardNo = rs.getString("cardNo"); 
- String Month = rs.getString("Month"); 
- String Year = rs.getString("Year"); 
- String ccvNo = rs.getString("ccvNo");
- // Add into the html table
- output += "<tr>  <td>" + cardID + "</td>"; 
- output += "<td>" + cardHolderName + "</td>"; 
- output += "<td>" + cardNo + "</td>"; 
- output += "<td>" + Month + "</td>"; 
- output += "<td>" + Year + "</td>";
- output += "<td>" + ccvNo + "</td>"; 
- // buttons
- output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"+ "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
- + "<input name='cardID' type='hidden' value='" + cardID 
- + "'>" + "</form></td></tr>"; 
- } 
- con.close(); 
- // Complete the html table
- output += "</table>"; 
- } 
- catch (Exception e) 
- { 
- output = "Error while reading the Card Details."; 
- System.err.println(e.getMessage()); 
- } 
- return output; 
- } 
+    return con;
+  }
 
+  public String insertCardData(
+    String name,
+    String cardno,
+    String month,
+    String year,
+    String ccvno
+  ) {
+    String output = "";
+    try {
+      Connection con = connect();
+      if (con == null) {
+        return "Error while connecting to the database for inserting.";
+      }
+      // create a prepared statement
+      String query =
+        " insert into carddata (`cardID`,`cardHolderName`,`cardNo`,`Month`,`Year`,`ccvNo`)" +
+        " values (?, ?, ?, ?, ? , ?)";
+      PreparedStatement preparedStmt = con.prepareStatement(query);
+      // binding values
+      preparedStmt.setInt(1, 0);
+      preparedStmt.setString(2, name);
+      preparedStmt.setString(3, cardno);
+      preparedStmt.setString(4, month);
+      preparedStmt.setString(5, year);
+      preparedStmt.setString(6, ccvno);
+      preparedStmt.execute();
+      con.close();
+      output = "Inserted successfully";
+    } catch (Exception e) {
+      output = "Error while inserting the carddata.";
+      System.err.println(e.getMessage());
+    }
+    return output;
+  }
 
-public String updateCard(String ID, String name, String cardno, String month, String year,String ccvno)
-{ 
-	 String output = ""; 
-	 try
-	 { 
-	 Connection con = connect(); 
-	 if (con == null) 
-	 {return "Error while connecting to the database for updating."; } 
-	 // create a prepared statement
-	 String query = "UPDATE carddata SET cardHolderName=?,cardNo=?,Month=?,Year=?,ccvNo=? WHERE cardID=?"; 
-	 PreparedStatement preparedStmt = con.prepareStatement(query); 
-	 // binding values
-	 
-	 preparedStmt.setString(1, name); 
-	 preparedStmt.setString(2, cardno); 
-	 preparedStmt.setString(3, month);
-	 preparedStmt.setString(4, year);
-	 preparedStmt.setString(5, ccvno);
-	 
-	 preparedStmt.setInt(6, Integer.parseInt(ID)); 
-	 // execute the statement
-	 preparedStmt.execute(); 
-	 con.close(); 
-	 output = "Updated successfully"; 
-	 } 
-	 catch (Exception e) 
-	 { 
-	 output = "Error while updating the Card Details."; 
-	 System.err.println(e.getMessage()); 
-	 } 
-	 return output; 
-	 } 
-	public String deleteCard(String itemID) 
-	 { 
-	 String output = ""; 
-	 try
-	 { 
-	 Connection con = connect(); 
-	 if (con == null) 
-	 {return "Error while connecting to the database for deleting."; } 
-	 // create a prepared statement
-	 String query = "delete from carddata where cardID=?"; 
-	 PreparedStatement preparedStmt = con.prepareStatement(query); 
-	 // binding values
-	 preparedStmt.setInt(1, Integer.parseInt(itemID)); 
-	 // execute the statement
-	 preparedStmt.execute(); 
-	 con.close(); 
-	 output = "Deleted successfully"; 
-	 } 
-	 catch (Exception e) 
-	 { 
-	 output = "Error while deleting the card details."; 
-	 System.err.println(e.getMessage()); 
-	 } 
-	 return output; 
-	 } 
-	
-	
+  public String readCardData() {
+    String output = "";
+    try {
+      Connection con = connect();
+      if (con == null) {
+        return "Error while connecting to the database for reading.";
+      }
+      // Prepare the html table to be displayed
+      output =
+        "<table border='1'><tr><th>Card ID</th><th>Card Holder Name</th><th>Card No</th>" +
+        "<th>Month</th>" +
+        "<th>Year</th>" +
+        "<th>CCV No</th>" +
+        "<th>Update</th><th>Remove</th></tr>";
 
-	
-	} 
+      String query = "select * from carddata";
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+      // iterate through the rows in the result set
+      while (rs.next()) {
+        String cardID = Integer.toString(rs.getInt("cardID"));
+        String cardHolderName = rs.getString("cardHolderName");
+        String cardNo = rs.getString("cardNo");
+        String Month = rs.getString("Month");
+        String Year = rs.getString("Year");
+        String ccvNo = rs.getString("ccvNo");
+        // Add into the html table
+        output += "<tr>  <td>" + cardID + "</td>";
+        output += "<td>" + cardHolderName + "</td>";
+        output += "<td>" + cardNo + "</td>";
+        output += "<td>" + Month + "</td>";
+        output += "<td>" + Year + "</td>";
+        output += "<td>" + ccvNo + "</td>";
+        // buttons
+        output +=
+          "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>" +
+          "<td><form method='post' action='items.jsp'>" +
+          "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>" +
+          "<input name='cardID' type='hidden' value='" +
+          cardID +
+          "'>" +
+          "</form></td></tr>";
+      }
+      con.close();
+      // Complete the html table
+      output += "</table>";
+    } catch (Exception e) {
+      output = "Error while reading the Card Details.";
+      System.err.println(e.getMessage());
+    }
+    return output;
+  }
 
- // execute the statement
+  public String updateCard(
+    String ID,
+    String name,
+    String cardno,
+    String month,
+    String year,
+    String ccvno
+  ) {
+    String output = "";
+    try {
+      Connection con = connect();
+      if (con == null) {
+        return "Error while connecting to the database for updating.";
+      }
+      // create a prepared statement
+      String query =
+        "UPDATE carddata SET cardHolderName=?,cardNo=?,Month=?,Year=?,ccvNo=? WHERE cardID=?";
+      PreparedStatement preparedStmt = con.prepareStatement(query);
+      // binding values
+
+      preparedStmt.setString(1, name);
+      preparedStmt.setString(2, cardno);
+      preparedStmt.setString(3, month);
+      preparedStmt.setString(4, year);
+      preparedStmt.setString(5, ccvno);
+
+      preparedStmt.setInt(6, Integer.parseInt(ID));
+      // execute the statement
+      preparedStmt.execute();
+      con.close();
+      output = "Updated successfully";
+    } catch (Exception e) {
+      output = "Error while updating the Card Details.";
+      System.err.println(e.getMessage());
+    }
+    return output;
+  }
+
+  public String deleteCard(String itemID) {
+    String output = "";
+    try {
+      Connection con = connect();
+      if (con == null) {
+        return "Error while connecting to the database for deleting.";
+      }
+      // create a prepared statement
+      String query = "delete from carddata where cardID=?";
+      PreparedStatement preparedStmt = con.prepareStatement(query);
+      // binding values
+      preparedStmt.setInt(1, Integer.parseInt(itemID));
+      // execute the statement
+      preparedStmt.execute();
+      con.close();
+      output = "Deleted successfully";
+    } catch (Exception e) {
+      output = "Error while deleting the card details.";
+      System.err.println(e.getMessage());
+    }
+    return output;
+  }
+}
+// execute the statement
